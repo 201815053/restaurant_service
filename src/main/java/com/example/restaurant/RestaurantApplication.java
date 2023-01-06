9fdfe40963f5b59d28d9d06711edb69b2a6f8e72
kyunghyun <rudgusee@gmail.com>
2023-01-06 23:05:45 +0900

    Create project

--- src/main/java/com/example/restaurant/RestaurantApplication.java
+++ src/main/java/com/example/restaurant/RestaurantApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RestaurantApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RestaurantApplication.class, args);
 	}
+
 	//기능들을 naver-api와 연동해서 memoryDb를 만들어서 연결을 시킴
 }
